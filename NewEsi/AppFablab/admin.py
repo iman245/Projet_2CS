8223912b972600ae3f08a16a92928b569a206d14
roumy <114481756+Roumy126@users.noreply.github.com>
2024-04-01 17:48:23 +0100

    Delete NewEsi directory

--- NewEsi/AppFablab/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Piece, Category, DemandeMateriel, FablabInscriptionUtilisateur
-
-admin.site.register(Piece)
-admin.site.register(Category)
-admin.site.register(DemandeMateriel)
-admin.site.register(FablabInscriptionUtilisateur)
