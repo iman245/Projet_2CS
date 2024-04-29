99bb5b7cff5e87f5b43083a88af436f57c9fe076
MerieemZitouni <km_zitouni@esi.dz>
2024-04-29 20:38:56 +0100

    fied some routes and read more clubs

--- src/pages/DetailsClubsFinale/DetailsClubsFinale.js
+++ src/pages/DetailsClubsFinale/DetailsClubsFinale.js
@@ -1,5 +1,6 @@
 import React, { useState, useMemo, useEffect } from "react";
 import PropTypes from "prop-types";
+import { Link } from 'react-router-dom';
 import cn from "classnames";
 import countryList from "react-select-country-list";
 import styles from "./DetailsClubsFinale.module.scss"
@@ -21,7 +22,11 @@ function ScrollToTop() {
 }
 
 function DetailsClubsFinale(props) {
- 
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+  };
 
   return (
     <div>
@@ -39,8 +44,8 @@ function DetailsClubsFinale(props) {
             </span>
           </span>
         </h1>
-        <h3 className={styles.descParagraph}>
-        L'École de l'IA d'Alger, fondée en 2018 à l'ESI Alger, est un club scientifique rassemblant des passionnés de l'intelligence artificielle. Son objectif est de permettre aux membres d'explorer leur potentiel dans ce domaine et d'améliorer leurs compétences en IA. 
+        <h3 className={cn(styles.descParagraph, { [styles.expanded]: expanded })}>
+        L'École de l'IA d'Alger, fondée en 2018 à l'ESI Alger, est un club scientifique rassemblant des passionnés de l'intelligence artificielle. Son objectif est de permettre aux membres d'explorer leur potentiel dans ce domaine et d'améliorer leurs compétences en IA. Le club propose des cours, des ateliers, des discussions et des événements pour aider les membres à se perfectionner dans ce domaine. Rejoignez-nous pour en savoir plus sur l'IA et pour participer à des projets passionnants! En reconnaissance de ses efforts pour promouvoir l'éducation et la recherche en intelligence artificielle, l'École de l'IA d'Alger a établi des partenariats avec des entreprises technologiques de premier plan, des institutions académiques renommées et des organisations gouvernementales. Ces collaborations permettent aux membres d'accéder à des ressources et des opportunités uniques telles que des stages, des projets de recherche appliquée et des programmes de mentorat, renforçant ainsi leur parcours professionnel et académique dans le domaine de l'IA. 
         </h3>
         </div>
       
@@ -48,17 +53,23 @@ function DetailsClubsFinale(props) {
       </div>
       <div className={styles.buttonsContainer}>
         <div className={styles.grpButton1}>
-       <button className={styles.joinButton}>
+          <Link to="https://linktr.ee/soai_algiers?fbclid=IwZXh0bgNhZW0CMTAAAR0qWw-8EDUF7fJgLxkGX8ll3RTFIZXyL6fWP5RAedUn6NQOic83ypboE6c_aem_AYahKtqssPEuYvVi6rr5XxGjRb0wPdUuuwfZiQA0un481MR0kxDqlpFftOzxIZvBGNFC7fFuZ1p9MUM2zXAjO3TK">
+          <button  className={styles.joinButton}>
         Nous rejoindre
        </button>
-       <div className={styles.regarderNousContainer}>
-       <img className={styles.regarderNousIcon} src={'/assets/regard.svg'} alt="alt text" />
+       </Link>
+      
+      
+      <div className={styles.regarderNousContainer}>
+      <Link to="https://www.youtube.com/@schoolofaialgiers">
+       <img className={styles.regarderNousIcon} src={'/assets/regard.svg'} alt="alt text" /></Link>
        Regardez Nous!
        </div>
+       
 
        </div>
 
-       <div className={styles.lirePlus}>Lire Plus
+       <div className={styles.lirePlus} onClick={toggleExpand}>{expanded ? "Lire moins" : "Lire plus"}
        <img className={styles.lirePlusArrow} src={'/assets/arrow.svg'} alt="alt text" />
        </div>
       </div>
