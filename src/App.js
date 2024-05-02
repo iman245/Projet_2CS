e3a3fb09adf3cfeb1379371abc0ba301f65bd449
MerieemZitouni <km_zitouni@esi.dz>
2024-05-02 19:57:13 +0100

    added club pages and Esi Final and fixed some fonts

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Alumni from './pages/Annuaire/Alumni';
 import Projets from './pages/Lmcs/Projets';
 import Equipe from './pages/Lmcs/Equipe';
 import Sidebar from './components/SidebarAdmin/Sidebar';
-
+import Clubs from './pages/Clubs/Clubs';
 
 import AvantPromo from './pages/AvantPromo/index'
 import CatalogueFormation from './pages/CatalogueFormations/index'
@@ -18,6 +18,8 @@ import DemandeEnregistree from './pages/DemandeDevis/DemandeEnreg'
 import DF from './pages/DF/index'
 import DemandePartenariatFinale from './pages/DemandePartenariatFinale/DemandePartenariatFinale';
 import DetailsClubsFinale from './pages/DetailsClubsFinale/DetailsClubsFinale';
+import ClubsFinal from './pages/ClubsFinal/ClubsFinal';
+import EsiFinal from './pages/EsiFinal/EsiFinal';
 
 function App() {
   return (
@@ -41,6 +43,9 @@ function App() {
         < Route path ="/DetailsFormation" element ={<DF/>}/>
         < Route path ="/DemandePartenariatFinale" element ={<DemandePartenariatFinale/>}/>
         < Route path ="/DetailsClubsFinale" element ={<DetailsClubsFinale/>}/>
+        <Route path='/Clubs' element={<Clubs/>}></Route>
+        < Route path ="/ClubsFinal" element ={<ClubsFinal/>}/>
+        <Route path='/EsiFinal' element={<EsiFinal/>}></Route>
         
 
 
