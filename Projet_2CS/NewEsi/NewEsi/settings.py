ac487a2145337ab1280f4c0cbe5a568d333607c4
SouhaSoukane <90262883+SouhaSoukane@users.noreply.github.com>
2024-06-19 23:51:16 +0200

    before deployement

--- Projet_2CS/NewEsi/NewEsi/settings.py
+++ Projet_2CS/NewEsi/NewEsi/settings.py
@@ -54,6 +54,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -157,6 +158,7 @@ ADMIN_EMAIL='kr_ghlib@esi.dz'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
