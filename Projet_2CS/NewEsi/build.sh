404539425db7aef7ec67c3968c2e394e5c4a17fd
SouhaSoukane <90262883+SouhaSoukane@users.noreply.github.com>
2024-06-20 00:35:48 +0200

    Update build.sh

--- Projet_2CS/NewEsi/build.sh
+++ Projet_2CS/NewEsi/build.sh
@@ -1,6 +1,8 @@
 set -o errexit
+REQUIREMENTS_FILE="./env/requirements.txt"
 
-pip install -r requirements.txt
+# Installer les dépendances à partir de requirements.txt
+pip install -r "$REQUIREMENTS_FILE"
 
 python manage.py collectstatic --no-input
 python manage.py migrate
