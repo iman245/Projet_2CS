bddeef05254587372019102382613ff017f8a670
SouhaSoukane <90262883+SouhaSoukane@users.noreply.github.com>
2024-06-20 02:48:02 +0200

    delete venv

--- Projet_2CS/NewEsi/build.sh
+++ Projet_2CS/NewEsi/build.sh
@@ -1,6 +1,11 @@
 set -o errexit
+REQUIREMENTS_FILE="./env/requirements.txt"
+MANAGE_FILE="Projet_2CS/NewEsi/manage.py"
+# Installer les dépendances à partir de requirements.txt
+pip install -r "$REQUIREMENTS_FILE"
 
-pip install -r requirements.txt
+# Collect static files
+python "$MANAGE_FILE" collectstatic --no-input
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+# Apply database migrations
+python "$MANAGE_FILE" migrate
