bf3a0f14970bf2038b6c70909960371d31439e5e
SouhaSoukane <90262883+SouhaSoukane@users.noreply.github.com>
2024-06-20 13:16:36 +0200

    Update build.sh

--- Projet_2CS/NewEsi/build.sh
+++ Projet_2CS/NewEsi/build.sh
@@ -1,11 +1,15 @@
 set -o errexit
 REQUIREMENTS_FILE="./env/requirements.txt"
-MANAGE_FILE="Projet_2CS/NewEsi/manage.py"
-# Installer les dépendances à partir de requirements.txt
+
+# Install dependencies from requirements.txt
 pip install -r "$REQUIREMENTS_FILE"
 
 # Collect static files
-python "$MANAGE_FILE" collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Apply database migrations
-python "$MANAGE_FILE" migrate
+python manage.py migrate
+
+# Start the application
+gunicorn Projet_2CS.NewEsi.NewEsi.wsgi:application
+
