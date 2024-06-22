3f59dd10f4fab4c9b9a2f27cdeed99c46877beb6
SouhaSoukane <90262883+SouhaSoukane@users.noreply.github.com>
2024-06-22 02:24:01 +0200

    Update build.sh

--- Projet_2CS/NewEsi/build.sh
+++ Projet_2CS/NewEsi/build.sh
@@ -7,13 +7,15 @@ pip install -r "$REQUIREMENTS_FILE"
 # Navigate to the directory containing manage.py
 cd Projet_2CS/NewEsi/
 
+
 # Collect static files
 python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py makemigrations
 python manage.py migrate
-
+python manage.py load_publication
+python manage.py load_enseignant_data.py
 
 
 
